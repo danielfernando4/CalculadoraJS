2f7ed2c6da0e715501db9565a4a958b9fe453369
DANIEL FERNANDO MERA CAMPOVERDE <daniel.mera01@epn.edu.ec>
2025-02-26 19:07:51 -0500

    validación y funcionalidad de las operciones matemáticas con js

--- calculadora.js
+++ calculadora.js
@@ -8,20 +8,52 @@ let cleanOneBtn = buttonsContainer[buttonsContainer.length - 3];
 let printField = document.getElementById("result-field");
 printField.value = ""
 
+let isAnswer = false;
+let isError = false;
+
 for (let i = 0; i < buttonsContainer.length - 3; i++) {
+
+    
     buttonsContainer[i].addEventListener("click", function() {
+        if(isAnswer || isError){
+            printField.value = "";
+            isAnswer = false;
+            isError = false;
+        }
+
         printField.value += this.textContent;
     });
 }
 
 cleanAllBtn.addEventListener("click", () => {
+    isError = false;
+    isAnswer = false;
     printField.value = "";
 });
 
 
 cleanOneBtn.addEventListener("click", () => {
-    if(printField.value > 0){
+    if(printField.value.length > 0){
+        if(isError){
+            printField.value = "";
+            isError = false;
+            isAnswer = false;
+        }
         printField.value = printField.value.slice(0, -1);
     }
+});
 
+
+equalBtn.addEventListener("click", () => {
+    const regex = /^\d+(\s*[-+*/]\s*\d+)*$/;
+    //alert(regex.test(printField.value));
+
+    if(regex.test(printField.value)){
+        const result = new Function("return " + printField.value);
+        printField.value = result();
+        isAnswer = true;
+    }else{
+        printField.value = "syntax error!!!";
+        isError = true;
+    }
 });
